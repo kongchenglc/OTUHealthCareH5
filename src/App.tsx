eb00b409957a7e5818928b67cc4ddcea07bfb653
kongchenglc <kongchenglc@gmail.com>
2024-10-11 20:14:22 -0400

    feat: first page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Chat from '@/components/Chat'
+
+function App() {
+
+  return <Chat />
+}
+
+export default App
