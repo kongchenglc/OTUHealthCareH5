affaec78f761d5502c8e851c95823b5846c3e823
kongchenglc <kongchenglc@gmail.com>
2024-11-17 18:43:32 -0500

    feat: health life advice

--- src/components/HealthLife.tsx
+++ src/components/HealthLife.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import ReactMarkdown from 'react-markdown';
 import './HealthLife.css';
 
